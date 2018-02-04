4ad99a97ffd2d8bf982e0ce966fd2b04af4f0bd9
Jason Yu <me@ycmjason.com>
2018-02-04 22:51:36 +0000

    fix for build

--- poi.config.js
+++ poi.config.js
@@ -7,6 +7,7 @@ module.exports = () => ({
     favicon: 'static/favicon.png',
     pkg: {}, // All package.json data
   },
+  transformModules: ['@faviator/create-svg-favicon'],
   webpack(config) {
     if (process.env.NODE_ENV === 'staging') {
       const RobotstxtPlugin = require('robotstxt-webpack-plugin').default;
