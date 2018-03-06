440a2f0d9e7801bab963752cdc81d7272258a7e8
Jason Yu <me@ycmjason.com>
2018-03-06 21:53:14 +0000

    1.2.1

--- poi.config.js
+++ poi.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = () => ({
   port: 8080,
   entry: './src/index.js',
@@ -6,11 +8,12 @@ module.exports = () => ({
     title: 'Faviator',
     description: 'A simple easy favicon generator.',
     favicon: 'static/favicon.png',
+    template: path.join(__dirname, 'index.ejs'),
     pkg: {}, // All package.json data
   },
   transformModules: ['@faviator/create-svg-favicon'],
   webpack(config) {
-    if (process.env.NODE_ENV === 'staging') {
+    if (process.env.NODE_ENV !== 'production') {
       const RobotstxtPlugin = require('robotstxt-webpack-plugin').default;
       config.plugins.push(new RobotstxtPlugin({
         policy: [
